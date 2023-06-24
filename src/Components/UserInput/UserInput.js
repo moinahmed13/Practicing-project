a7566005594714d4743d625319e67f071d9d22ec
Moin Ahmed <ahmedmoin234@gmail.com>
2023-06-24 19:56:33 +0200

    inoutChangeHandler modified

--- src/Components/UserInput/UserInput.js
+++ src/Components/UserInput/UserInput.js
@@ -7,17 +7,29 @@ const UserInput = () => {
     event.preventDefault();
   };
 
-  const changeHandler = () => {};
+  const inputChangeHandler = (input, value) => {};
   return (
     <form onSubmit={submitHandler} className="form">
       <div className="input-group">
         <p>
           <label htmlFor="current-savings">Current Savings ($)</label>
-          <input type="number" id="current-savings" />
+          <input
+            onChange={(event) =>
+              inputChangeHandler("current-savings", event.target.value)
+            }
+            type="number"
+            id="current-savings"
+          />
         </p>
         <p>
           <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-          <input type="number" id="yearly-contribution" />
+          <input
+            onChange={(event) =>
+              inputChangeHandler("yearly-contribution", event.target.value)
+            }
+            type="number"
+            id="yearly-contribution"
+          />
         </p>
       </div>
       <div className="input-group">
@@ -25,11 +37,23 @@ const UserInput = () => {
           <label htmlFor="expected-return">
             Expected Interest (%, per year)
           </label>
-          <input onChange={changeHandler} type="number" id="expected-return" />
+          <input
+            onChange={(event) =>
+              inputChangeHandler("expected-return", event.target.value)
+            }
+            type="number"
+            id="expected-return"
+          />
         </p>
         <p>
           <label htmlFor="duration">Investment Duration (years)</label>
-          <input onChange={changeHandler} type="number" id="duration" />
+          <input
+            onChange={(event) =>
+              inputChangeHandler("duration", event.target.value)
+            }
+            type="number"
+            id="duration"
+          />
         </p>
       </div>
       <p className="actions">
