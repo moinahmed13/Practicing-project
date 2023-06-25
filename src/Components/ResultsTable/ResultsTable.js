de857bfae67dc564976f05467486e4efcfdd8d32
Moin Ahmed <ahmedmoin234@gmail.com>
2023-06-25 16:04:10 +0200

    adding CSS modules

--- src/Components/ResultsTable/ResultsTable.js
+++ src/Components/ResultsTable/ResultsTable.js
@@ -1,5 +1,4 @@
-import "./ResultsTable.css";
-
+import classes from "./ResultsTable.module.css";
 const formatter = new Intl.NumberFormat("en-US", {
   style: "currency",
   currency: "USD",
@@ -9,7 +8,7 @@ const formatter = new Intl.NumberFormat("en-US", {
 
 const ResultsTable = (props) => {
   return (
-    <table className="result">
+    <table className={classes.result}>
       <thead>
         <tr>
           <th>Year</th>
